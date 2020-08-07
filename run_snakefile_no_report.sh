41b22bfd97bef5c7d10d8e9f2d25e0c0c8e21f81
Wen Luo <wen.luo@nih.gov>
2020-08-06 22:27:01 -0400

    update fastq, relatedness check module and bug fix

--- run_snakefile_no_report.sh
+++ run_snakefile_no_report.sh
@@ -6,9 +6,9 @@ DATE=$(date +%y%m%d)
 mkdir -p logs_${DATE}
 
 
-snakemake --unlock  
+snakemake --unlock -s Snakefile_no_report --configfile modules/config_no_report.yaml
 
 sbcmd="qsub -cwd -q {cluster.q} -pe by_node {threads} -o logs_${DATE}/ -e logs_${DATE}/ -V"
-qsub -cwd -q all.q -N run_Snakefile_no_report  -o logs_${DATE}/Snakefile_no_report.stdout -e logs_${DATE}/Snakefile_no_report.stderr -b y "module load python3 sge R/3.4.0 gcc zlib;snakemake -pr -s Snakefile_no_report --keep-going --rerun-incomplete --local-cores 1 --jobs 1000 --configfile modules/config_no_report.yaml --cluster \"$sbcmd\" --cluster-config cluster.yaml --latency-wait 120 all"
+qsub -cwd -q seq-calling.q -N run_Snakefile_no_report  -o logs_${DATE}/Snakefile_no_report.stdout -e logs_${DATE}/Snakefile_no_report.stderr -b y "module load python3 sge R/3.4.0 gcc zlib;snakemake -pr -s Snakefile_no_report --keep-going --rerun-incomplete --local-cores 1 --jobs 1000 --configfile modules/config_no_report.yaml --cluster \"$sbcmd\" --cluster-config cluster.yaml --latency-wait 120 all"
 
 
